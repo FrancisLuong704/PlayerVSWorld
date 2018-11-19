443d293a6aa9182dc4ac9997076131961b3a56d5
YoungFrank <fluong95@gmail.com>
2018-11-19 14:00:28 -0500

    dash

--- player-vs-world/client/src/components/pages/Dashboard.js
+++ player-vs-world/client/src/components/pages/Dashboard.js
@@ -3,7 +3,7 @@ import "./Dashboard.css";
 
 const Dashboard = () => (
   // page container
-  <div className="uk-container-large page-container">
+  <div className="uk-container page-container">
     {/* title and dashboard title container */}
     <div className="uk-padding-medium">
       <h1 className="uk-heading-primary pvw">Player VS World</h1>
@@ -12,21 +12,21 @@ const Dashboard = () => (
     {/* card container */}
     <div className="cardRows">
       {/* friend card */}
-      <div className="uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
+      <div className="uk-width-1-3@s cardBody uk-grid-collapse uk-grid uk-margin-small-left">
         <div className="uk-card uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Friends</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
       {/* game card */}
-      <div className="uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
+      <div className="uk-width-1-3@s cardBody uk-grid-collapse uk-grid uk-margin-small-left">
         <div className="uk-card uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Games</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
       {/* group card */}
-      <div className="uk-card uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
+      <div className="uk-card uk-width-1-3@s cardBody uk-grid-collapse uk-grid uk-margin-small-left">
         <div className="uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Groups</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>

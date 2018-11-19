7b2b499b54d3f9f1a2cdf5bd3cf3b5e6e2557519
YoungFrank <fluong95@gmail.com>
2018-11-19 13:40:40 -0500

    background

--- player-vs-world/client/src/components/pages/Dashboard.js
+++ player-vs-world/client/src/components/pages/Dashboard.js
@@ -12,21 +12,21 @@ const Dashboard = () => (
     {/* card container */}
     <div className="cardRows">
       {/* friend card */}
-      <div className="uk-child-width-1-1@s uk-grid-collapse uk-grid">
+      <div className="uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
         <div className="uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Friends</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
       {/* game card */}
-      <div className="uk-child-width-1-1@s uk-grid-collapse uk-grid">
+      <div className="uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
         <div className="uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Games</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
       {/* group card */}
-      <div className="uk-child-width-1-1@s uk-grid-collapse uk-grid">
+      <div className="uk-child-width-1-1@s cardBody uk-grid-collapse uk-grid">
         <div className="uk-card-transparent dashboardCards uk-card-default uk-card-hover uk-card-body">
           <h2 className="uk-card-title">My Groups</h2>
           <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>

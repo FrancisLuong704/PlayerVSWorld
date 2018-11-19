f64e4d10fe1341970958a58c0f0c4c6cee29a9a9
Joxel <joxelstudios@gmail.com>
2018-11-19 14:45:19 -0500

    update

--- player-vs-world/client/src/components/pages/MakeLink.js
+++ player-vs-world/client/src/components/pages/MakeLink.js
@@ -1,11 +1,24 @@
 import React from "react";
+import "./MakeLink.css"
 
 const MakeLink = () => (
-    <div className="uk-container-large">
+    <div className="uk-container">
         {/* title and profile title container */}
-        <div className="uk-padding-medium">
+        <div className="uk-padding-large">
             <h1 className="uk-heading-primary pvw">Player VS World</h1>
-            <h1 className="newLink">Create a New Forum</h1>
+            <h1 className="uk-heading-line heading"><span>Create a new post</span></h1>
+            <div className="uk-card uk-card-default">
+                <div className="uk-card-body">
+                <form>
+                    Related Game:
+                    <select className="uk-select uk-margin-small ">
+                        <option>Game</option>
+                    </select>
+                    <textarea className="uk-textarea uk-margin-small">Type Content Here</textarea>
+                    <button className="uk-button-default uk-margin-small" type="button">SUBMIT</button>               
+                </form>
+                </div>
+            </div>
         </div>
     </div>
 );

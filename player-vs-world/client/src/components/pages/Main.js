e1d13ad3c0a4e8eaac399248032ef408b82b4fc0
YoungFrank <fluong95@gmail.com>
2018-11-22 17:55:15 -0500

    so here we have working create blog and displaying all blogs

--- player-vs-world/client/src/components/pages/Main.js
+++ player-vs-world/client/src/components/pages/Main.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../../utils/API";
 import "./Main.css";
-const Main = () => (
 
-    <div className="uk-container-large">
-        <h1 className="uk-heading-primary uk-margin-medium-left mainPVW">Player VS World</h1>
-        <div className="uk-container uk-width-1-3">
-        </div>
-        <div className="uk-container uk-width-1-3">
-        </div>
-        <div className="uk-container uk-width-1-3">
-        </div>
+class Main extends Component {
+    state = {
+        blogs: []
+    }
 
-    </div>
+    componentDidMount() {
+        this.blogFind();
+    }
 
-);
+    blogFind = () => {
+        API.blogFind()
+            .then(res => this.setState({ blogs: res.data, title: "", game: "", content: "" }))
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <div className="uk-container-large">
+                <h1 className="uk-heading-primary uk-margin-medium-left mainPVW">Player VS World</h1>
+                <div className="uk-container uk-width-1-3">
+                    {this.state.blogs.length ? (
+                        <div>
+                            {this.state.blogs.map(blog => (
+                                <div key={blog.id}>
+
+                                    <h1> {blog.title} </h1>
+
+                                    <h2> {blog.game}</h2>
+
+                                    <h3> {blog.content} </h3>
+
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                            <h3>No Results to Display</h3>
+                        )}
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Main;
+
+

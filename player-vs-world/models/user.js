e004a831ec2a84eaca80032f1d8a6beed2777fb8
Breezy <pbrowning11@gmail.com>
2018-11-27 01:37:06 -0500

    branch check

--- player-vs-world/models/user.js
+++ player-vs-world/models/user.js
@@ -34,6 +34,14 @@ module.exports = function (sequelize, DataTypes) {
       validate: {
         len: [1]
       }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
     }
     
   });
